ac532084ef749e9a0dbc7d07a9cf93998bc3f1b8
Christian Aquino <cjfaquino@gmail.com>
2022-04-10 04:39:19 -0700

    add function to draw on canvas

--- scripts.js
+++ scripts.js
@@ -1,13 +1,16 @@
+let mouseDown = false;
+document.addEventListener('mousedown', () => {mouseDown = true})
+document.addEventListener('mouseup', () => {mouseDown = false})
 
 function newDiv(num) {  
   const cont = document.querySelector('.container');
   const div = document.createElement('div');
   div.style.height = "100%";
   div.style.width = "100%";
-  div.style.backgroundColor = "red";
   div.style.border = "1px solid"
   cont.style = `grid-template-columns: repeat(${num}, 1fr)`
   cont.appendChild(div);
+  div.addEventListener('mouseenter', draw)
 }
 
 function canvasSize(num) {
@@ -25,3 +28,11 @@ function clearDiv(container) {
       cont.removeChild(cont.firstChild)
   }
 }
+
+function draw() {
+  if(mouseDown){
+    this.style.backgroundColor = "black"
+  }
+}
+
+canvasSize(32)
